80a05b7542b682543dcab7ecbb4237fce25e4e46
Charles <cking90@gatech.edu>
2020-01-13 14:22:36 -0500

    Add ability to route to different components

--- excel-tracker-application/src/components/App.js
+++ excel-tracker-application/src/components/App.js
@@ -1,46 +1,43 @@
 import React, { Component } from 'react';
-import logo from './../assets/images/logo.svg';
 import './../styles/App.css';
+
 
 import ACTIONS from '../redux/action'
 import { connect } from 'react-redux';
+import Home from './Home/Home.js';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
 
-  componentDidMount() {
-    this.props.setCurrentUser("amy@gmail.com")
-    console.log("Props", this.props)
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello, {this.props.currentUser}
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router> 
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
-});
-const mapDispatchToProps = dispatch => {
-  return {
-    setCurrentUser : (currentUser) => dispatch(ACTIONS.setCurrentUser(currentUser))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
