ec3f01c25fb7c413962e4afa4a1368b53cd34908
Fernanda Moreno <fmoreno6@gatech.edu>
2020-01-20 18:34:20 -0500

    created card for competencies

--- excel-tracker-application/src/components/Home/Home.js
+++ excel-tracker-application/src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './../../assets/images/logo.svg';
+import competencies from './../../assets/images/competencies.png'
 import './../../styles/Home.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 
 import ACTIONS from '../../redux/action'
@@ -18,10 +21,7 @@ class Home extends Component {
       <div className="Home">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello, user. <br></br>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Dashboard</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -30,6 +30,19 @@ class Home extends Component {
           >
             Hello, {this.props.currentUser}
           </a>
+          <br></br>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={competencies} />
+            <Card.Body>
+              <Card.Title>Competencies</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <br></br>
         </header>
       </div>
     );
