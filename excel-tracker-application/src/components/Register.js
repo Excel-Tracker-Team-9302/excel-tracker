794473f4025c3ec6d1c4ad208ce3b34a3da68de1
BradenChapman <bchapman34@gatech.edu>
2020-01-22 14:08:40 -0500

    Added functionality to button and text boxes

--- excel-tracker-application/src/components/Register.js
+++ excel-tracker-application/src/components/Register.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
-import { Divider } from '@material-ui/core';
+import './../styles/Register.css';
+import BasicButton from './Common/BasicButton';
 
 class Register extends Component {
+    handleClick() {
+        alert("I was clicked!");
+    }
     render() {
         return(
-            <div>Hello</div>
+            <div className='Register'>
+              <div>Enter new username: <input type='text' placeholder='Username'/></div>
+              <div>Enter new password: <input type='text' placeholder='Password'/></div>
+              <BasicButton label={'Register'} onClick={this.handleClick}/>
+            </div>
         );
     }
 }
