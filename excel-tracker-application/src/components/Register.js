3d9ec47b32587ad5dded35d350da90b58f469c10
mattbusch <m.busch345@gmail.com>
2020-01-24 13:10:26 -0500

    Makes Register look more like login

--- excel-tracker-application/src/components/Register.js
+++ excel-tracker-application/src/components/Register.js
@@ -1,32 +1,88 @@
-import React, {Component} from 'react';
-import './../styles/Register.css';
-import BasicButton from './Common/BasicButton';
+import React, { Component } from 'react';
+import './../styles/Login.css';
+import ResetPasswordButton from './Common/ResetPasswordButton.js';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Colors from '../assets/text/Colors.js';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 import {
   withRouter
 } from 'react-router-dom'
 
+import ACTIONS from '../redux/action'
+import { connect } from 'react-redux';
+
 
 class Register extends Component {
 
-    constructor(props) {
-      super(props);
+  username = '';
+  password = '';
+  
 
-      this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick() {
-        this.props.history.push('/');
-    }
-    render() {
-        return(
-            <div className='Register'>
-              <h1>Register a new user</h1>
-              <div>Enter new username: <input type='text' placeholder='Username'/></div>
-              <div>Enter new password: <input type='password' placeholder='Password'/></div>
-              <BasicButton label={'Register'} onClick={this.handleClick}/>
-            </div>
-        );
-    }
+  handleChangeUsername = event => {
+    this.username = (event.target.value || '');
+  };
+
+  handleChangePassword = event => {
+    this.password = (event.target.value || '');
+  };
+
+  handleRegister = event => {
+    this.props.history.push('/login');
+  };
+
+  handleCancel = event => {
+    this.props.history.push('/login');
+  };
+
+  render() {
+
+    return (
+      <div className="Login">
+        <header className="Login-header">
+          <div style={{color:Colors.blue}}>
+            Register a new user
+          </div>
+        </header>
+        <div className="Login-body">
+          <form className = "Login-form">
+            <div className='Login-input' style={{color:Colors.blue}}>Username: <input type="text" className="Login-box" onChange={this.handleChangeUsername}/></div>
+            <div className='Login-input' style={{color:Colors.blue}}>Password: <input type="password" className="Login-box" onChange={this.handleChangePassword}/></div>
+
+            <Button style={styles.button} onClick={this.handleRegister}>Register</Button>
+            <Button style={styles.button} onClick={this.handleCancel}>Cancel</Button>
+
+
+          </form>        
+        </div>
+
+      </div>
+    );
+  }
 }
 
-export default withRouter(Register);
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+});
+const mapDispatchToProps = dispatch => {
+  return {
+    setCurrentUser : (currentUser) => dispatch(ACTIONS.setCurrentUser(currentUser))
+  }
+}
+
+const styles = {
+  button: {
+    color:Colors.white, 
+    'background-color':Colors.blue, 
+    'border-radius':'50pt', 
+    width: 'calc(30px + 20vmin)',
+    margin:'10px', 
+    padding:'10px', 
+    'font-size': 'calc(5px + 3vmin)'
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Register));
