042b2702131f43ac443b0eee9d1879ba3a8f857c
sairapoonnen <sairapoonnen@gmail.com>
2020-02-03 14:07:33 -0500

    Add filtering

--- excel-tracker-application/src/components/Users.js
+++ excel-tracker-application/src/components/Users.js
@@ -8,12 +8,18 @@ import {
 import Header from './Home/Header.js';
 import {dummyData} from './../services/DummyData';
 import UserList from './UserView/UserList';
-
+import { InputLabel } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { MenuItem} from '@material-ui/core';
 
 class Users extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+        selected: null
+    }
 
     this.handleLogout = this.handleLogout.bind(this);
   }
@@ -21,21 +27,38 @@ class Users extends React.Component {
   handleLogout() {
     this.props.history.push('/');
   }
-
+  handleChange(value) {
+    this.setState({selected: value });
+  }
   render() {
       return (
+        
         <div>
           <Header handleLogout={() => this.handleLogout} pageName="Users"/>
           <div className="Users">
           
+          <InputLabel id="label">Filter by</InputLabel>
+            <Select labelId="label" 
+            id="select" 
+            value={this.state.selected}
+            onChange={event => this.handleChange(event.target.value)}>
+            <MenuItem value="All">All</MenuItem>
+            <MenuItem value="Student">Student</MenuItem>
+            <MenuItem value="Mentor">Mentor</MenuItem>
+            <MenuItem value="Faculty/Staff">Faculty/Staff</MenuItem>
+            <MenuItem value="Admin">Admin</MenuItem>
+            </Select>
 
           <div className='tc'>
+              
           <h1>UserList</h1>
-          <UserList users={dummyData.users} /> 
+          <UserList users={dummyData.users.filter(user => {
+    return user.role === this.state.selected || this.state.selected == "All";
+})} /> 
           </div>
           
           <br></br>
-          
+         
           </div>
         </div>
         
