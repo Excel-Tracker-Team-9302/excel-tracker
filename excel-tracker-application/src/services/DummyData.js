484aa8e3d988689fee2d6cbf8bf1afbcd8309d2f
Charles <cking90@gatech.edu>
2020-02-02 20:30:04 -0500

    Create some competencies and users

---
+++ excel-tracker-application/src/services/DummyData.js
@@ -0,0 +1,76 @@
+dummyData = {
+
+  competencies: {
+    1: {
+      title: "Can call an Uber/Lyft",
+      domain: "Transportation",
+      subcategory: "Uber/Lyft",
+      difficulty: "I",
+      frequency: "Sem"
+    },
+    2: {
+      title: "Can cancel an Uber/Lyft",
+      domain: "Transportation",
+      subcategory: "Uber/Lyft",
+      difficulty: "A",
+      frequency: "Sem"
+    },
+    3: {
+      title: "Is on time for work/internships",
+      domain: "Employment/Careers",
+      subcategory: "Planning and Preparation",
+      difficulty: "I",
+      frequency: "Mnth"
+    },
+    4: {
+      title: "Performs assigned tasks and follows directions on the job",
+      domain: "Employment/Careers",
+      subcategory: "Work Skills",
+      difficulty: "I",
+      frequency: "Sem"
+    },
+    5: {
+      title: "Understands and utilizes advanced benefits: health insurance, Employee Assistance Programs (EAP)",
+      domain: "Employment/Careers",
+      subcategory: "Work Skills",
+      difficulty: "E",
+      frequency: "Yr"
+    },
+    6: {
+      title: "Addresses bullying through effective communication by discussing the situation with others, or asking for help if needed",
+      domain: "Social and Leadership Development",
+      subcategory: "General Social Skills",
+      difficulty: "A",
+      frequency: "Sem"
+    }
+  },
+
+  users: {
+    "cking32@gatech.edu": {
+      role: "Faculty/Staff",
+      name: "Cole King"
+    },
+
+    "asullivan@gatech.edu": {
+      name: "Andy Sullivan",
+      role: "Student",
+      cohort_number: 2,
+      mentor: null,
+      status: "current"
+    },
+
+    "psullivan@gatech.edu": {
+      name: "Perry Sullivan",
+      role: "Student",
+      cohort_number: 2,
+      mentor: null,
+      status: "current"
+    },
+
+    "bdylan9@gatech.edu": {
+      name: "Bob Dylan",
+      role: "Mentor",
+      student: "asullican@gatech.edu",
+    }
+  }
+}
