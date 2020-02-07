32510248720b410b289118532bb2fece76a7de83
Fernanda Moreno <fmoreno6@gatech.edu>
2020-02-07 17:24:20 -0500

    spring cleaning on coolcard code and dashboard

--- excel-tracker-application/src/components/Home/coolcard.js
+++ excel-tracker-application/src/components/Home/coolcard.js
@@ -7,13 +7,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import CardColumns from 'react-bootstrap/Card';
-
-import competencies from './../../assets/images/competencies.png'
-import reports from './../../assets/images/reports.png'
-import users from './../../assets/images/users.png'
-import courses from './../../assets/images/courses.png'
-import {Container, Row, Col } from 'react-bootstrap'
 import {withStyles } from '@material-ui/core/styles';
 
 import {
@@ -31,103 +24,33 @@ const useStyles = makeStyles({
   },
 });
 function ImgMediaCard(props) {
-  const classes = useStyles();
+
+  console.log(props);
+  const classes = props.classes;
+  console.log(classes);
+
+  const styles = useStyles();
 
   return (
     <div>
 
-    <Container fixed style = {{ alignItems: 'center', display: 'grid', 'grid-template-columns': '300px 300px', 'justify-content':'center', 'grid-gap': '10px'}}>
-    <Card className={classes.card}>
+    <Card className={styles.card}>
       <CardActionArea style = {{height: '80%'}}>
         <CardMedia
           component="img"
-          alt="Competencies"
+          alt={props.pageName}
           height= "100%"
-          image={competencies}
-          title="Competencies"
+          image={props.image}
+          title={props.pageName}
+          onClick={props.onClick}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Competencies
+            {props.pageName}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="black">
-          View
-        </Button>
-      </CardActions>
     </Card>
-
-  <Card className={classes.card}>
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        alt="Users"
-        height="100%"
-        image={users}
-        title="Users"
-        onClick={() => props.history.push('/users')}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Users
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <Button size="small" color="black">
-        View
-      </Button>
-    </CardActions>
-  </Card>
-
-  <Card className={classes.card}>
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        alt="Courses"
-        height="100%"
-        image={courses}
-        title="Courses"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Courses
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <Button size="small" color="black">
-        View
-      </Button>
-    </CardActions>
-  </Card>
-
-  <Card className={classes.card}>
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        alt="Reports"
-        height="100%"
-        image={reports}
-        title="Reports"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Reports
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <Button size="small" color="black">
-        View
-      </Button>
-    </CardActions>
-  </Card>
-
-  </Container>
-
   </div>
   );
 }
