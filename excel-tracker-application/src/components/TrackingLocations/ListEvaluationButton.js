a88e2082b9f6db368b8b88edc7804d113360d750
mattbusch <m.busch345@gmail.com>
2020-04-15 01:09:21 -0400

    Inline Evaluations for Tracking Location Page

---
+++ excel-tracker-application/src/components/TrackingLocations/ListEvaluationButton.js
@@ -0,0 +1,38 @@
+import IconButton from '@material-ui/core/IconButton';
+import React, { Component } from 'react';
+import ReviewIcon from '@material-ui/icons/RateReview'
+
+
+
+/**
+ * Stylized evaluation button icon. Uses the passed in function prop
+ * to determine functionality
+ * 
+ * (April 2020)
+ */
+class ListEvaluationButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick = () => {
+    if (this.props.handleClick) {
+      this.props.handleClick()
+    }
+  }
+
+
+  render() {
+    return (
+      <div classname="cardStyle" style={{ height: '50%', maxWidth: '5%', margin: '0.8vh 1vw 1vh 0.1vw' }}>
+        <IconButton onClick={this.handleClick}>
+          <ReviewIcon />
+        </IconButton>
+      </div>
+    );
+  }
+
+}
+
+export default (ListEvaluationButton);
